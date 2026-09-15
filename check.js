3ec858987d4cfabb334642ca2f1c74ca49b4f9c0
haziq1121 <haziq1121@users.noreply.github.com>
2026-09-15 11:48:48 +0800

    Remove utility/dev files

--- check.js
+++
@@ -1,7 +0,0 @@
-﻿const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-let out = '';
-out += 'isAbout: ' + html.split('<sc-if value=\"{{ isAbout }}\">').length + '\n';
-out += 'isProjects: ' + html.split('<sc-if value=\"{{ isProjects }}\">').length + '\n';
-out += 'isContact: ' + html.split('<sc-if value=\"{{ isContact }}\">').length + '\n';
-fs.writeFileSync('check.txt', out);
